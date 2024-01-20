4b8de7f161c58f33fb2a4b88a56ace2fffd437ed
Jingon Kim <rhslrhs@gmail.com>
2024-01-20 23:30:48 +0900

    add. test api 2

--- src/main/java/com/singularity/home/web/HomeRestController.java
+++ src/main/java/com/singularity/home/web/HomeRestController.java
@@ -3,11 +3,13 @@ package com.singularity.home.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -17,5 +19,16 @@ public class HomeRestController {
         String now = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
         log.debug("## now: {}", now);
         return ResponseEntity.ok(now);
+    }
+    @GetMapping("/wait")
+    public ResponseEntity<Map<String, Object>> wait(@RequestParam(defaultValue = "1000") Long time) throws InterruptedException {
+        Map<String, Object> res = new HashMap<>();
+        res.put("reqTime", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        String now = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+        log.debug("## wait-time: {}", time);
+        Thread.sleep(time);
+        res.put("resTime", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        res.put("time", time);
+        return ResponseEntity.ok(res);
     }
 }
